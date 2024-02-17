e6fecd874583168b783c57bc968a69baf605a9bc
FuexFollets <fuex9follets@gmail.com>
2024-02-17 14:44:05 -0500

    Implemented training methods for word encoder

--- src/efuzz/train_encoder.hpp
+++ src/efuzz/train_encoder.hpp
@@ -68,7 +68,7 @@ namespace efuzz {
         TrainingResult train(const StringT& string_1, const StringT& string_2);
         TrainingResult train(const std::vector<std::pair<StringT, StringT>>& string_pairs);
         TrainingResult train_random(std::size_t iterations);
-        TrainingResult train_all(std::size_t iterations);
+        TrainingResult train_all();
 
         this_type& modify_encoder(const NeuralNetwork::NeuralNetworkDiff& diff);
 
@@ -210,52 +210,96 @@ namespace efuzz {
             throw std::runtime_error("No dataset provided");
         }
 
-        if (_dataset->empty()) {
-            throw std::runtime_error("Empty dataset provided");
+        if (_dataset->size() < 2) {
+            throw std::runtime_error("Dataset too small");
         }
 
-        const std::size_t dataset_size = _dataset->size();
+        std::vector<std::pair<StringT, StringT>> string_pairs;
+
+        std::random_device random_device;
+        std::mt19937 random_engine(random_device());
+        std::uniform_int_distribution<std::size_t> distribution(0, _dataset->size() - 1);
+
+        for (std::size_t iteration = 0; iteration < iterations; ++iteration) {
+            const std::size_t index_1 = distribution(random_engine);
+            const std::size_t index_2 = distribution(random_engine);
+
+            if (index_1 == index_2) {
+                continue;
+            }
+
+            string_pairs.emplace_back((*_dataset.value()) [index_1], (*_dataset.value()) [index_2]);
+        }
+
+        return train(string_pairs);
+    }
+
+    template <typename StringT_>
+    TrainEncoder<StringT_>::TrainingResult
+        TrainEncoder<StringT_>::train_all() {
+        if (!_dataset) {
+            throw std::runtime_error("No dataset provided");
+        }
+
+        if (_dataset->size() < 2) {
+            throw std::runtime_error("Dataset too small");
+        }
+
+        // Dont create string pairs, that takes too much memory
 
         const NeuralNetwork original_encoder_nn = _encoder.get_word_vector_encoder_nn();
-        std::random_device random_device;
-        std::mt19937 random(random_device());
 
         float average_cost_of_unmodified_encoder {};
 
-        for (std::size_t iteration {}; iteration < iterations; ++iteration) {
-            const NeuralNetwork::NeuralNetworkDiff random_diff =
-                _encoder.get_word_vector_encoder_nn().random_diff();
-            const StringT& string_1 = _dataset->at(random() % dataset_size);
-            const StringT& string_2 = _dataset->at(random() % dataset_size);
+        // Use nested for loops. Indexes should be indexer_1, indexer_2
 
-            const float cost = cost(string_1, string_2);
+        for (std::size_t indexer_1 = 0; indexer_1 < _dataset->size(); ++indexer_1) {
+            for (std::size_t indexer_2 = 0; indexer_2 < _dataset->size(); ++indexer_2) {
+                if (indexer_1 == indexer_2) {
+                    continue;
+                }
 
-            average_cost_of_unmodified_encoder += cost;
+                const float cost = TrainEncoder<StringT_>::cost((*_dataset.value()) [indexer_1],
+                                                                (*_dataset.value()) [indexer_2]);
+
+                average_cost_of_unmodified_encoder += cost;
+            }
         }
 
-        average_cost_of_unmodified_encoder /= iterations;
+        const std::size_t comparisons = _dataset->size() * (_dataset->size() - 1);
+        average_cost_of_unmodified_encoder /= comparisons;
 
         NeuralNetwork::NeuralNetworkDiff diff = original_encoder_nn.random_diff();
+
         _encoder.get_word_vector_encoder_nn().modify(diff);
 
         float average_cost_of_modified_encoder {};
 
-        for (std::size_t iteration {}; iteration < iterations; ++iteration) {
-            const StringT& string_1 = _dataset->at(random() % dataset_size);
-            const StringT& string_2 = _dataset->at(random() % dataset_size);
+        for (std::size_t indexer_1 = 0; indexer_1 < _dataset->size(); ++indexer_1) {
+            for (std::size_t indexer_2 = 0; indexer_2 < _dataset->size(); ++indexer_2) {
+                if (indexer_1 == indexer_2) {
+                    continue;
+                }
 
-            const float cost = cost(string_1, string_2);
+                const float cost = TrainEncoder<StringT_>::cost((*_dataset.value()) [indexer_1],
+                                                                (*_dataset.value()) [indexer_2]);
 
-            average_cost_of_modified_encoder += cost;
+                average_cost_of_modified_encoder += cost;
+            }
         }
 
         _encoder.set_word_vector_encoder_nn(original_encoder_nn);
 
-        if (average_cost_of_unmodified_encoder < average_cost_of_modified_encoder) {
+        average_cost_of_modified_encoder /= comparisons;
+
+        if (average_cost_of_modified_encoder < average_cost_of_unmodified_encoder) {
             return TrainingResult {.diff = diff,
                                    .original_cost = average_cost_of_unmodified_encoder,
                                    .modified_cost = average_cost_of_modified_encoder};
         }
+
+        return TrainingResult {.original_cost = average_cost_of_unmodified_encoder,
+                               .modified_cost = average_cost_of_modified_encoder};
     }
 } // namespace efuzz
 
